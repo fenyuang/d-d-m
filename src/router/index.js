d8669784ffb8164d5c2d1ea7e3dabbcdf515a6fc
Sophia <15889570065@163.com>
2017-11-09 22:20:35 +0800

    upata

--- src/router/index.js
+++ src/router/index.js
@@ -5,16 +5,13 @@ import header from '@/components/header'
 import recruit from '@/components/recruit'
 import JoinUs from '@/components/JoinUs'
 import companyIntro from '@/components/companyIntro'
-<<<<<<< HEAD
 import ContactUs from '@/components/ContactUs'
 import FQA from '@/components/FQA'
 import aboutUs from '@/components/aboutUs'
 
-=======
 import fqa from '@/components/fqa'
 import ContactUs from '@/components/ContactUs'
 import aboutUs from '@/components/aboutUs'
->>>>>>> f6b83c7ead298ba22f702474c79967671a71ec0a
 Vue.use(Router)
 
 export default new Router({
@@ -49,27 +46,15 @@ export default new Router({
       name: 'ContactUs',
       component: ContactUs
     },
-<<<<<<< HEAD
-    {
-      path: '/FQA',
-      name: 'FQA',
-      component: FQA
-    },
-=======
->>>>>>> f6b83c7ead298ba22f702474c79967671a71ec0a
     {
       path: '/aboutUs',
       name: 'aboutUs',
       component: aboutUs
-<<<<<<< HEAD
-=======
     },
     {
       path: '/fqa',
       name: 'fqa',
       component: fqa
->>>>>>> f6b83c7ead298ba22f702474c79967671a71ec0a
     }
-
   ]
 })
